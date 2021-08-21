a12f99ceeae2a80b4fa4ded4a2ba1f61734b8966
CN-Koileo <49806659+liewoi@users.noreply.github.com>
2021-08-21 16:05:34 +0800

    Update and rename pixiv50.py to pixiv.py

--- pixiv.py
+++ pixiv.py
@@ -2,7 +2,7 @@
 import sys
 from you_get import common as you_get
 print("Welcome to Pixiv Images Downloads!\nIt is come from Leo!")
-
+where = input("输入你要爬取的Pixiv的下载地址")
 year = input("输入你要爬取的Pixiv日榜年:")
 month = input("输入你要爬取的Pixiv日榜月:")
 day = input("输入你要爬取的Pixiv日榜日:")
@@ -12,13 +12,13 @@ if year == '0' :
     exit()
 elif month == '0' :
     url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/'
-    directory = r'/root/pixiv/' + year + '/'
+    directory = r where + year + '/'
 elif day == '0' :
     url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/'
-    directory = r'/root/pixiv/' + year + '/' + month + '/'
+    directory = r where + year + '/' + month + '/'
 else:
     url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' + day +'_daily/'
-    directory = r'/root/pixiv/' + year + '/' + month + '/'+ day +'_daily/'
+    directory = r where + year + '/' + month + '/'+ day +'_daily/'
 
 sys.argv = ['you-get','-o',directory,url]
 you_get.main()
