519867559eb1276b4643975d046be6050c2df18a
CN-Koileo <49806659+liewoi@users.noreply.github.com>
2021-08-21 17:56:56 +0800

    Update pixiv.py

--- pixiv.py
+++ pixiv.py
@@ -15,23 +15,52 @@ elif month == '0' :
     i = 1
     j = 1
     while i <= 12:
-        while j <= 31:
-            url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + str(i) + '/' + str(j) +'_daily/'
-            sys.argv = ['you-get','-o',directory,url]
-            you_get.main()
+        if(i<10):
+            if(j<10):
+                url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + '0' + month + '/' + '0' + str(f) +'_daily/'
+                os.makedirs('https://pic.tjsky.net/pixiv/pic/' + year + '/' + '0' + month + '/' + '0' + str(f) +'_daily/')
+                directory = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + '0' +  month + '/' + '0' + str(f) +'_daily/'
+                sys.argv = ['you-get','-o',directory,url]    
+                you_get.main()
+            elif(j>=10):
+                url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + '0' + month + '/' + str(f) +'_daily/'
+                os.makedirs('https://pic.tjsky.net/pixiv/pic/' + year + '/' + '0' + month + '/' +  + str(f) +'_daily/')
+                directory = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + '0' + month + '/' +  + str(f) +'_daily/'
+                sys.argv = ['you-get','-o',directory,url]    
+                you_get.main()
+            j+=1
+        if(i>=10):
+            if(j<10):
+                url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + str(i) + '/' + '0' + str(j) +'_daily/'
+                os.makedirs(where + year + '/'+ str(i) + '/' + '0' + str(j) +'_daily/')
+                directory = where + year + '/'+ str(i) + '/' + '0' + str(j) +'_daily/'
+                sys.argv = ['you-get','-o',directory,url]
+                you_get.main()
+            elif(j>=10):
+                url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + str(i) + '/' + str(j) +'_daily/'
+                os.makedirs(where + year + '/'+ str(i) + '/' + str(j) +'_daily/')
+                directory = where + year + '/'+ str(i) + '/' + str(j) +'_daily/'
+                sys.argv = ['you-get','-o',directory,url]
+                you_get.main()    
             j+=1
         i+=1
-    os.makedirs(where + year + '/')
-    directory = where + year + '/'
 elif day == '0' :
     f=1
     while f<31:
-        url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' + str(f) +'_daily/'
-        sys.argv = ['you-get','-o',directory,url]
-        you_get.main()
+        if(f<10):
+            url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' + '0' + str(f) +'_daily/'
+            os.makedirs('https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' + '0' + str(f) +'_daily/')
+            directory = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' + '0' + str(f) +'_daily/'
+            sys.argv = ['you-get','-o',directory,url]    
+            you_get.main()
+        elif(f>=10):
+            url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' + str(f) +'_daily/'
+            os.makedirs('https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' +  + str(f) +'_daily/')
+            directory = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' +  + str(f) +'_daily/'
+            sys.argv = ['you-get','-o',directory,url]    
+            you_get.main()
         f+=1
-    os.makedirs(where + year + '/' + month + '/')
-    directory = where + year + '/' + month + '/'
+
 else:
     url = 'https://pic.tjsky.net/pixiv/pic/' + year + '/' + month + '/' + day +'_daily/'
     os.makedirs(where + year + '/' + month + '/'+ day +'_daily/')
